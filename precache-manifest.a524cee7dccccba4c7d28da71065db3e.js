c96f6db760d286cc2d1d5ac84eceb6b491a56903
zooplankton <33819748+zooplankton@users.noreply.github.com>
2019-08-02 16:36:09 -0400

    Updates

---
+++ precache-manifest.a524cee7dccccba4c7d28da71065db3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cb68757e2994811247f412ef1f88df0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "af433f1b318600bb0048",
+    "url": "/monsters-rolodex/static/css/main.1d1f5a80.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "af433f1b318600bb0048",
+    "url": "/monsters-rolodex/static/js/main.ac0acc3a.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
